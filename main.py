fa2ab4d4a96292f53e4042631d6071f9377fbec0
msi <meibrohimov.@gmail.com>
2024-01-20 15:32:05 +0500

    sql

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 from aiogram.filters.command import Command
 from root import TOKEN
+import sqlite3
+
+DATABASE_FILE = 'user_db.sqlite'
+
+conn = sqlite3.connect(DATABASE_FILE)
+cursor = conn.cursor()
+
+# Create a table if it doesn't exist
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        full_name VARCHAR(255),
+        username VARCHAR(255),
+        user_id INTEGER
+    )
+''')
+conn.commit()
 
 
 bot = Bot(token=TOKEN)
@@ -13,6 +30,10 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def start(message: Message):
     await message.answer("Hello there!")
+    cursor.execute('INSERT INTO messages (full_name, username, user_id) VALUES (?, ?, ?)',
+                   (message.from_user.full_name, message.from_user.username, message.from_user.id))
+    conn.commit()
+    await message.answer("send your telegram data")
 
 
 async def main():
